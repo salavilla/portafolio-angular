d35d901bc2f98389371508d0bddd1a435d44ba21
Irvin Salazar <salazarirvin4@gmail.com>
2019-05-19 23:03:23 -0600

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,9 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
