e84e328cd0b71268e80e621154af4a6c468626da
Irvin Salazar <salazarirvin4@gmail.com>
2019-05-26 21:34:21 -0600

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,22 +8,42 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
     //console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+   
 
-
-    //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
+    private cargarInfo(){
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+      
 
 
 
       //console.log(resp['email']);
-    })
+    });
+
+
+    }
+
+      private cargarEquipo(){
+        this.http.get('https://angular-html-ad025.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+      
+      this.equipo = resp;
+      //console.log(resp);
+
+
+
+      //console.log(resp['email']);
+    });
+      }
 
    }
-}
+
